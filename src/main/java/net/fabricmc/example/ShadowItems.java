5cac589f2fc3b213fbba5db3318ebcf177c0050d
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-06-07 08:45:55 -0700

    Update ShadowItems.java

--- src/main/java/net/fabricmc/example/ShadowItems.java
+++ src/main/java/net/fabricmc/example/ShadowItems.java
@@ -3,6 +3,17 @@ package net.fabricmc.example;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.block.Blocks;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.UniformLootTableRange;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootTableEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.util.Identifier;
 
 public class ShadowItems implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
@@ -10,6 +21,9 @@ public class ShadowItems implements ModInitializer {
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger("shadowitems");
 
+	//Mod ID
+    	public static final String MOD_ID = "shadowitems";
+	
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -18,4 +32,19 @@ public class ShadowItems implements ModInitializer {
 
 		LOGGER.info("Hello Fabric world! Shadow Items Has Loaded!");
 	}
+	
+    	public static final ItemGroup EXPLOITS_GROUP = FabricItemGroupBuilder.create(
+            	new Identifier(MOD_ID, "exploits"))
+            	.icon(() -> new ItemStack(Blocks.ARMOR_STAND))
+            	.build();
+
+    	public static final ItemGroup GRIEF_GROUP = FabricItemGroupBuilder.create(
+            	new Identifier(MOD_ID, "grief"))
+            	.icon(() -> new ItemStack(Blocks.TNT))
+            	.build();
+
+    	public static final ItemGroup SPECIAL_GROUP = FabricItemGroupBuilder.create(
+            	new Identifier(MOD_ID, "special"))
+            	.icon(() -> new ItemStack(Blocks.STRUCTURE_VOID))
+            	.build();	
 }
