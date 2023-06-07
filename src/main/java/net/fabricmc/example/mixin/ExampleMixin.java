210b4fb5011127c189286247a43ff61c4dde01ab
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-06-07 09:08:13 -0700

    Update ExampleMixin.java

--- src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
+++ src/main/java/net/fabricmc/example/mixin/ExampleMixin.java
@@ -11,6 +11,6 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class ExampleMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by a mod mixin!");
+		ShadowItems.LOGGER.info("This line is printed by a mod mixin!");
 	}
 }
