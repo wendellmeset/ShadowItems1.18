9016db165ca5a0d0135597588f3192d2a9e82022
Wendellmeset <69725697+wendellmeset@users.noreply.github.com>
2023-06-07 08:32:58 -0700

    Update ShadowItems.java

--- src/main/java/net/fabricmc/example/ShadowItems.java
+++ src/main/java/net/fabricmc/example/ShadowItems.java
@@ -4,11 +4,11 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class ShadowItems implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("shadowitems");
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +16,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("Hello Fabric world! Shadow Items Has Loaded!");
 	}
 }
